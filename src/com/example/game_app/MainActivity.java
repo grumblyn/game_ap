d84db9c9ed9d71daa9d82e9b6cb9500c951f54de
Robert Stewart <rcstewart@smu.edu>
2013-12-05 18:25:03 -0600

    trying to get movement to work correctly.  One step at a time

--- src/com/example/game_app/MainActivity.java
+++ src/com/example/game_app/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.game_app;
 
-import android.animation.Animator;
-import android.animation.Animator.AnimatorListener;
 import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
-
+	private boolean changedirx;
+	private boolean changediry;
+	private float currentx;
+	private float currenty;
+	private float tempx;
+	private float tempy;
+	private float x;
+	private float y;
+	private FrameLayout ball;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,18 +34,18 @@ public class MainActivity extends Activity {
 		Display display = getWindowManager().getDefaultDisplay();
 		DisplayMetrics outMetrics = new DisplayMetrics ();
 		display.getMetrics(outMetrics);
-
-		float density  = getResources().getDisplayMetrics().density;
-		float dpHeight = outMetrics.heightPixels / density;
-		float dpWidth  = outMetrics.widthPixels / density;
 		
+		x = 200;
+		y = 0;
+		currentx=0;
+		currenty=0;
+		
+		ball = (FrameLayout) findViewById(R.id.ball);
 		Button launcher = (Button) findViewById(R.id.launch);
 		launcher.setOnClickListener(new View.OnClickListener() {
 		    @Override
 		    public void onClick(View v) {
-			    float changex = 110;
-			    float changey = -100;
-		          launch(v, changex, changey);
+		          launch();
 		    }
 		});
 	}
@@ -49,92 +58,99 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
+	
 
-	public void launch(View v, float x, float y) {
-		FrameLayout ball = (FrameLayout) findViewById(R.id.ball);
+//	public void launch(View v, float x, float y, float currentx, float currenty) {
+	public void launch() {
+		//ball = (FrameLayout) findViewById(R.id.ball);
 		RelativeLayout parent = (RelativeLayout) findViewById(R.id.parent);
-		float currentx = 0;
-		float currenty = 0;
-		boolean changedirx = false;
-		boolean changediry = false;
+		//float currentx = 0;
+		//float currenty = 0;
+		
+		changedirx = false;
+		changediry = false;
 		
 		int maxw = parent.getWidth();
 		int maxh = parent.getHeight();
 		maxw = maxw - ball.getWidth();
 		maxh = maxh - ball.getHeight();
 		AnimatorSet set = new AnimatorSet();
-		while (1 < Math.abs(x) || 1 < Math.abs(y)){
-			boolean xisdone = false;
-			boolean yisdone = false;
-			float tempx = x;
-			float tempy = y;
-			if(ball.getLeft() + x <= 0) {
-				changedirx = true;
-				tempx = (-1)*ball.getLeft();
-			}
-			if(ball.getLeft() + x >= maxw) {
-				changedirx = true;
-				tempx = maxw - currentx;
-				
-			}
-			if(ball.getTop() + y <= 0) {
-				changediry = true;
-				tempy = ball.getTop();
-			}
-			if(ball.getTop() + y >= maxh) {
-				changediry = true;
-				tempy = maxh - currenty;
-			}
-			ObjectAnimator xani = new ObjectAnimator();
-			ObjectAnimator yani = new ObjectAnimator();
-			xani.addListener(new AnimatorListener() {
-				    @Override 
-				    public void onAnimationEnd(Animator animation) {
-				                
-				    }
-
-				@Override
-                        public void onAnimationCancel(Animator animation) {
-	                        // TODO Auto-generated method stub
-	                        
-                        }
-
-				@Override
-                        public void onAnimationRepeat(Animator animation) {
-	                        // TODO Auto-generated method stub
-	                        
-                        }
-
-				@Override
-                        public void onAnimationStart(Animator animation) {
-	                        // TODO Auto-generated method stub
-	                        
-                        }
-				});
-//			set.playTogether(
-//			    ObjectAnimator.ofFloat(ball, "translationX", currentx, tempx),
-//			    ObjectAnimator.ofFloat(ball, "translationY", currenty, tempy)
-//			);
-			set.playTogether(
-					xani.ofFloat(ball, "translationX", currentx, tempx),
-					yani.ofFloat(ball, "translationY", currenty, tempy)
-			);
-			set.setDuration(3000).start();
-			//set direction changes
-			if(changedirx){
-				x = x*(-1);
-			}
-			if(changediry){
-				y = y*(-1);
-			}
+		//while (1 < Math.abs(x) || 1 < Math.abs(y)){
+			tempx = x;
+			tempy = y;
+//			if(ball.getLeft() + x <= 0) {
+//				changedirx = true;
+//				tempx = (-1)*ball.getLeft();
+//			}
+//			if(ball.getLeft() + x >= maxw) {
+//				changedirx = true;
+//				tempx = maxw - ball.getLeft();
+//				
+//			}
+//			if(ball.getTop() + y <= 0) {
+//				changediry = true;
+//				//tempy = ball.getTop();
+//				tempy = (-1)*ball.getTop();
+//			}
+//			if(ball.getTop() + y >= maxh) {
+//				changediry = true;
+//				tempy = maxh - ball.getTop();
+//			}
 			
-			currentx = currentx + tempx;
-			currenty = currenty - tempy;
-			x = (float) (.7*x);
-			y = (float) (.7*y);
-			changedirx = false;
-			changediry = false;
-		}
+			//Animation animation = new TranslateAnimation(currentx, tempx,currenty, tempy);
+			Animation animation = new TranslateAnimation(
+					Animation.ABSOLUTE, ball.getLeft(), 
+					Animation.ABSOLUTE, tempx, 
+					Animation.ABSOLUTE, ball.getTop(), 
+					Animation.ABSOLUTE, tempy);
+			//Animation animation = new TranslateAnimation(ball.getLeft(), tempx,ball.getTop(), tempy);
+			animation.setDuration(1000);
+			animation.setFillBefore(false);
+			animation.setFillEnabled(true);
+			animation.setFillAfter(true);
+			
+			animation.setAnimationListener(new AnimationListener(){
+
+				@Override
+                        public void onAnimationEnd(Animation animation) {
+	                        // TODO Auto-generated method stub
+					//set direction changes
+					currentx = currentx + x;
+					if(true == true){
+						x = -x;
+					}
+					if(changediry == true){
+						y = y*(-1);
+					}
+					//FrameLayout ball = (FrameLayout) findViewById(R.id.ball);
+					int checkx = ball.getLeft();
+					int checky = ball.getTop();
+					ball.setTop(checky);
+					ball.setLeft(checkx);
+					
+					//int temp_tempx = (int) tempx*ball.getWidth();
+					
+					//currenty = currenty - tempy;
+					//x = (float) (.6*x);
+					y = (float) (.6*y);
+					Toast.makeText(getApplicationContext(), ""+currentx, Toast.LENGTH_SHORT).show();
+					if(x >= .05 || y >= .05){
+					//	launch();
+					}
+                        }
+
+				@Override
+                        public void onAnimationRepeat(Animation animation) {
+	                        // TODO Auto-generated method stub
+	                        
+                        }
+
+				@Override
+                        public void onAnimationStart(Animation animation) {
+	                        // TODO Auto-generated method stub
+	                        
+                        }});
+			ball.startAnimation(animation);
 		
 	}
 	
